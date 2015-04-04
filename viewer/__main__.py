a31d36ac349c29ec97230387a241a64891c47867
Gyumin Sim <sim0629@gmail.com>
2015-04-05 01:42:53 +0900

    picking/showing-all animation 중에는 배경 칠해주기

--- viewer/__main__.py
+++ viewer/__main__.py
@@ -36,9 +36,14 @@ def motion(x, y):
   camera.motion(x, y)
 
 def display():
+  camera.update()
+  if camera.is_animating():
+    glClearColor(0.5, 0.5, 0.5, 0.0)
+  else:
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-  camera.update()
   glMatrixMode(GL_MODELVIEW)
   model.render()
 

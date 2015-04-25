66e38f260af35ccc05b7f76f6b5ee5609b10762b
Gyumin Sim <sim0629@gmail.com>
2015-04-26 01:19:48 +0900

    cross section 점 편집 기능 구현

--- surface/cross.py
+++ surface/cross.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
 
@@ -12,14 +13,28 @@ data = None
 idx = 0
 steps = 10
 
+class Drag:
+  def __init__(self):
+    self.ing = False
+  def begin(self, point):
+    self.ing = True
+    self.source = point
+  def move(self, point):
+    self.target = point
+  def end(self):
+    self.ing = False
+drag = Drag()
+
 def keyboard(ch, x, y):
   global idx, steps
   if ch == ']':
     idx = (idx + 1) % data.n
     refresh_title()
+    drag.end()
   elif ch == '[':
     idx = (idx - 1) % data.n
     refresh_title()
+    drag.end()
   elif ch == '+':
     steps = steps + 1
   elif ch == '-':
@@ -27,10 +42,17 @@ def keyboard(ch, x, y):
       steps = steps - 1
 
 def mouse(button, state, x, y):
-  pass
+  if state == GLUT_DOWN:
+    drag.begin(np.array([x, y]))
+    pick_point()
+  elif state == GLUT_UP:
+    drag.end()
 
 def motion(x, y):
-  pass
+  if not drag.ing:
+    return
+  drag.move(np.array([x, y]))
+  move_point()
 
 def display():
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -54,6 +76,24 @@ def display():
     glVertex(p[0], 0.0, p[1])
   glEnd()
 
+def pick_point():
+  ring = data.points[idx]
+  for i in xrange(len(ring)):
+    v = np.array(ring[i]) - drag.source
+    d = np.sqrt(v.dot(v))
+    if d < 0.05:
+      drag.picked_i = i
+      drag.picked_p = ring[i]
+      return
+  drag.picked_i = None
+
+def move_point():
+  if drag.picked_i is None:
+    return
+  x = drag.target - drag.source
+  ring = data.points[idx]
+  ring[drag.picked_i] = list(np.array(drag.picked_p) + x)
+
 def refresh_title():
   title.change('Cross Section #%d' % idx)
 
@@ -73,4 +113,5 @@ def start():
   gluOrtho2D(-1.0, 1.0, -1.0, 1.0)
 
   refresh_title()
+  drag.end()
 

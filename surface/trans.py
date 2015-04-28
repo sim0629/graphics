2389d54d3f6ce2b2a2776d604fc23924576f068b
Gyumin Sim <sim0629@gmail.com>
2015-04-28 11:46:59 +0900

    필요없는 normalize 제거

--- surface/trans.py
+++ surface/trans.py
@@ -147,9 +147,8 @@ def move_point():
     a = camera._nearplane_point(drag.target, False) - camera.pos
     b = camera._nearplane_point(drag.source, False) - camera.pos
     q = qt.from_two_vectors(a, b)
-    o = Quaternion.pow(Vector.from_list(drag.picked_rotation[1:]).normalize(), drag.picked_rotation[0])
-    q = o * q
-    v = Quaternion.ln(q)
+    o = Quaternion.pow(Vector.from_list(drag.picked_rotation[1:]), drag.picked_rotation[0])
+    v = Quaternion.ln(q * o)
     data.rotations[drag.picked_i] = [v.length()] + v.normalize().to_list()
 
 def refresh_title():
